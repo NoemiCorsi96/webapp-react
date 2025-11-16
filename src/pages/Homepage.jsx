e1d7ae0909d42be39ee4adf17d31dd689ab922c6
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-11-16 11:10:06 +0100

    Add movies list rendering using data from AJAX call

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
-import { use, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 //const API_URL = import.meta.env.VITE_API_URL;
 const API_URL = "http://localhost:3000/api/movies";
 export default function Homepage() {
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    console.log("movies:", movies);
     useEffect(() => {
         axios.get(API_URL)
             .then(res => {
                 console.log(res);
+                setMovies(res.data.movie);
 
             })
             .catch(err => {
                 console.log(err.message);
+                setError(err.message)
             })
     }, [])
 
@@ -28,7 +33,37 @@ export default function Homepage() {
 
                 </div>
             </div>
+            <section className="mb-4">
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-md-3 g-4">
+                        {movies.map(movie => (
+                            <div className="col" key={movie.id}>
+                                <div className="card">
+                                    <Link to={`/movies/${movie._id}`}>
+                                        <img className="card-img-top" src={movie.image} alt={movie.title} />
+                                    </Link>
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            {movie.title}
+                                        </h5>
+                                        <div className="my-2">
+                                            {movie.genre}
+                                        </div>
+                                        <Link className="btn btn-dark" to={`/movies/${movie._id}`}></Link>
 
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
+                    <div className="text-center">
+                        <Link className="btn btn-dark mt-5">View Details</Link>
+                    </div>
+                </div>
+
+            </section>
         </>
     )
 }
